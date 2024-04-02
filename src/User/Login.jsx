933f8c41afb771815417a284aaa57dac6f259a6d
mithul2004 <mithulbala2006@gmail.com>
2024-04-02 22:36:46 +0530

    merged

--- src/User/Login.jsx
+++ src/User/Login.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Login.css";
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const navigate = useNavigate();
@@ -51,25 +49,6 @@ const Login = () => {
           </a>
         </form>
       </div>
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label htmlFor="username">UserName:</label>
-          <input type="text" id="email" value={username} onChange={(e) => setusername(e.target.value)} required />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-        </div>
-        <div>
-          <button type="submit">Login</button>
-        </div>
-        <button onClick={handleclick}>
-          Don't have an account ! sign up
-        </button>
-      </form>
-    </div>
     </div>
   );
 }
