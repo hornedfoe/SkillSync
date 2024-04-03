365ff57ac823aff7c2ef5f737afcb03abf1ddefa
mithul2004 <mithulbala2006@gmail.com>
2024-04-03 21:31:46 +0530

    small c

--- src/User/Changepassword.jsx
+++ src/User/Changepassword.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import './Changepassword.css'
+import './changepassword.css'
 import axios from 'axios';
 
 const Changepassword = () => {
