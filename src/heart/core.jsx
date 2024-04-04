eb95022fa758ad269f5940c4d8758dc0ff7166c5
sakthii25 <sakthinarayananr25@gmail.com>
2024-04-04 12:27:45 +0530

    sd

--- src/heart/core.jsx
+++ src/heart/core.jsx
@@ -1,10 +1,32 @@
-import react from 'react'
+import React from 'react';
+import './Core.css'; // Import CSS file for styling
 
-const Core = ()=>{
-    const fetch = localStorage.getItem('userData');
-    return(
-        <div>{console.log(fetch)}</div>
-    )
-}
+const LeftComponent = () => {
+    return (
+        <div className="left">Left Component</div>
+    );
+};
+
+const CenterComponent = () => {
+    return (
+        <div className="center">Center Component</div>
+    );
+};
+
+const RightComponent = () => {
+    return (
+        <div className="right">Right Component</div>
+    );
+};
+
+const Core = () => {
+    return (
+        <div className="core">
+            <LeftComponent />
+            <CenterComponent />
+            <RightComponent />
+        </div>
+    );
+};
 
 export default Core;
