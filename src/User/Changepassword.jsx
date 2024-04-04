707bb1682bc3884937efb3031ded9b57cf8eb7f6
sakthii25 <sakthinarayananr25@gmail.com>
2024-04-04 09:37:53 +0530

    o

--- src/User/Changepassword.jsx
+++ src/User/Changepassword.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import './changepassword.css'
+import './Changepassword.css'
 import axios from 'axios';
 
 const Changepassword = () => {
